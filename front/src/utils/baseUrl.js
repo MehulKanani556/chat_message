e5c00255af5ee757fcd18ee97cef739f30e84796
AkshayP-kalathiya <padaliya.kalathiyainfotech@gmail.com>
2025-04-18 16:29:44 +0530

    url change

--- front/src/utils/baseUrl.js
+++ front/src/utils/baseUrl.js
@@ -1,2 +1,2 @@
-export const BASE_URL =`http://localhost:5000/api`
-export const IMG_URL =`http://localhost:5000/`
+export const BASE_URL =`https://chat-message-0fml.onrender.com/api`
+export const IMG_URL =`https://chat-message-0fml.onrender.com/`
