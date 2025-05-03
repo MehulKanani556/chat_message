91316a45d785f5d1f4cb1966c2004136b92c2b62
archit bhuva <architbhuva23@gmail.com>
2025-05-03 12:14:15 +0530

    videocall

--- front/src/utils/baseUrl.js
+++ front/src/utils/baseUrl.js
@@ -1,4 +1,4 @@
  export const BASE_URL =`https://chat-message-0fml.onrender.com/api`
  export const IMG_URL =`https://chat-message-0fml.onrender.com/`
-//export const BASE_URL = `http://localhost:5000/api`
-//export const IMG_URL = `http://localhost:5000/`
+// export const BASE_URL = `http://localhost:5000/api`
+// export const IMG_URL = `http://localhost:5000/`
